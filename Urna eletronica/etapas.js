4b0728e5f93e6ff49a1c1da25f84f4394f6d6713
MarceloAlves7 <94135645+MarceloAlves7@users.noreply.github.com>
2022-03-26 14:16:46 -0300

    Urna eletronica
    Enviando Projeto-"Urna eletrônica" terminada

---
+++ Urna eletronica/etapas.js
@@ -0,0 +1,56 @@
+let etapas=[
+    {
+    titulo: 'Vereador',
+    numeros: 5,
+    cadidatos:  [
+            {
+                numero:'22022',
+                nome:'Trunks',
+                partido:'DBGT',
+                fotos:[
+                    {url:'trunks.jpg', legenda:'Vereador'}
+                ]
+            }  
+        ]
+
+    },
+    
+    
+    {titulo: 'Prefeito',
+    numeros: 2,
+    cadidatos: 
+        [
+            {
+                numero:'17',
+                nome:'Piccolo',
+                partido:'DBZ',
+                vice:'Gohan',
+                fotos:
+                [
+                    {url:'Piccolo.jpg', legenda:'Prefeito'},
+                    {url:'gohan.png', legenda:'Vice-Prefeito',small:true}
+                ]
+            }   
+        ]
+
+    },
+
+    {titulo: 'Presidente',
+    numeros: 2,
+    cadidatos: 
+        [
+            {
+                numero:'22',
+                nome:'Goku',
+                partido:'DBS',
+                vice:'Vegeta',
+                fotos:
+                [
+                    {url:'Goku.jpg', legenda:'Presidente'},
+                    {url:'vegeta.jpg', legenda:'Vice-Presidente',small:true}
+                ]
+            }   
+        ]
+
+    }
+]
